6002f8b263643448190063c0d8e290aa07d0d21e
Gus <gusta1296@gmail.com>
2020-03-07 22:18:18 -0800

    first commit

---
+++ src/build/precache-manifest.41a74003be6e20e662e519b4b8a917a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd3159b9966c597dd33171648948a75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7973e51a59213c362c5",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "ef4b0a3db0a5331a2b25",
+    "url": "/static/js/2.8d4374fb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.8d4374fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7973e51a59213c362c5",
+    "url": "/static/js/main.afbb8fd5.chunk.js"
+  },
+  {
+    "revision": "fc6b0834b26c11c7b958",
+    "url": "/static/js/runtime-main.742f9f3b.js"
+  }
+]);
